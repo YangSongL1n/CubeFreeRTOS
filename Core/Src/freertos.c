525934b90616580f0d84cec1f1d566d25dbbc817
yang songlin <104891109+YangSongL1n@users.noreply.github.com>
2024-06-11 16:31:19 +0800

    s

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -25,11 +25,26 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include "adc.h"
 #include "T5UIC1.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
+typedef enum{
+	H_AD_STATUS_0	= 1 ,//0V
+	H_AD_STATUS_6 = 2 ,//6V
+	H_AD_STATUS_9 = 3 ,//9V
+	H_AD_STATUS_12 =4 ,//12V
+	H_AD_STATUS_ERR = 5 //未知电压
+} CP_AD_STATUS;
+typedef struct
+{
+  CP_AD_STATUS cp_ad_status;
+	uint16_t  cp_ad_voltage;
+
+}Adc_data;
 
 /* USER CODE END PTD */
 
@@ -61,6 +76,13 @@ const osThreadAttr_t LCD_attributes = {
   .stack_size = 256 * 4,
   .priority = (osPriority_t) osPriorityLow,
 };
+/* Definitions for AdcTask */
+osThreadId_t AdcTaskHandle;
+const osThreadAttr_t AdcTask_attributes = {
+  .name = "AdcTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
@@ -69,6 +91,7 @@ const osThreadAttr_t LCD_attributes = {
 
 void StartTask(void *argument);
 void LCDTask(void *argument);
+void StartAdc03(void *argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
@@ -105,6 +128,9 @@ void MX_FREERTOS_Init(void) {
   /* creation of LCD */
   LCDHandle = osThreadNew(LCDTask, NULL, &LCD_attributes);
 
+  /* creation of AdcTask */
+  AdcTaskHandle = osThreadNew(StartAdc03, NULL, &AdcTask_attributes);
+
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
   /* USER CODE END RTOS_THREADS */
@@ -125,13 +151,12 @@ void MX_FREERTOS_Init(void) {
 void StartTask(void *argument)
 {
   /* USER CODE BEGIN StartTask */
+
   /* Infinite loop */
   for(;;)
   {
     HAL_GPIO_TogglePin(LED_B_GPIO_Port,LED_B_Pin);
-
     osDelay(1000);
-    printf("StartTask\r\n");
   }
   /* USER CODE END StartTask */
 }
@@ -148,15 +173,42 @@ void LCDTask(void *argument)
   /* USER CODE BEGIN LCDTask */
   DWINLCD_Init(USART3);
   DWINLCD_Clear(White);
-	DWINLCD_ShowXChar(1, 1, A64, Black, White, 30, 50, "Vol:");
+	DWINLCD_ShowXChar(1, 1, A64, Black, White, 30, 50, "Num:");
+  uint32_t number=0;
   /* Infinite loop */
   for(;;)
   {
-    osDelay(1);
-    printf("LCDTask\r\n");
-    osDelay(250);
+  // DWINLCD_ShowXChar(1, 1, 0x05, Black, White, 30, 210, "hello");
+  DWINLCD_ShowXNum(1, 1, 1, 0, A64, Black, White, 4, 0, 125,50, number++);
+  osDelay(1000);
   }
   /* USER CODE END LCDTask */
+}
+
+/* USER CODE BEGIN Header_StartAdc03 */
+/**
+* @brief Function implementing the AdcTask thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartAdc03 */
+void StartAdc03(void *argument)
+{
+  /* USER CODE BEGIN StartAdc03 */
+  
+  HAL_ADCEx_Calibration_Start(&hadc1);    //ADC内部校准
+  HAL_ADC_Start(&hadc1);    //ADC开启转换
+  uint16_t ADC_V =0;
+  /* Infinite loop */
+  for(;;)
+  {
+    uint16_t ADC_num = HAL_ADC_GetValue(&hadc1);    //获取ADC端口数据
+ 
+    ADC_V = 3300*ADC_num/4096;   
+    printf("\r\n%d",ADC_V);
+    // osDelay(500);
+  }
+  /* USER CODE END StartAdc03 */
 }
 
 /* Private application code --------------------------------------------------*/
